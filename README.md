14d2946f970c203d067671a16a401e970b588aea
AxulityGD <axulity34@outlook.com>
2019-11-30 22:20:33 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# js-zbgtst
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/js-zbgtst)
---
+++ index.js
@@ -0,0 +1,10 @@
+// Import stylesheets
+import './style.css';
+
+var html = "page";
+
+var follow = "webs"
+
+console.log(html)
+
+alert('This website is in Beta!!!')
